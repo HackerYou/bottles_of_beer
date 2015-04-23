31971309066eba239be3fd3bd2786404d65aba0a
Ryan Brunner <ryan@influitive.com>
2015-04-23 19:00:53 -0400

    4 to 3

--- bottles.rb
+++ bottles.rb
@@ -4,10 +4,16 @@ class Bottles
       "No more bottles of beer on the wall, no more bottles of beer. Go to the store, get some more, 99 bottles of beer on the wall."
     elsif number == 1
       "1 bottle of beer on the wall, 1 bottle of beer. Take one down, pass it around, no more bottles of beer on the wall."
-    elsif number == 2
-      "2 bottles of beer on the wall, 2 bottles of beer. Take one down, pass it around, 1 bottle of beer on the wall."
     else
-      "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down, pass it around, #{number - 1} bottles of beer on the wall."
+      "#{number} bottles of beer on the wall, #{number} bottles of beer. Take one down, pass it around, #{number - 1} #{bottle_noun(number - 1)} of beer on the wall."
+    end
+  end
+
+  def bottle_noun(number)
+    if number == 1
+      "bottle"
+    else
+      "bottles"
     end
   end
 
